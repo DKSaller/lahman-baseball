17b8421930cdaba3d1f83ce72735b4d3b9e1ba51
Donald Saller <donaldksaller@gmail.com>
2025-04-29 07:47:48 -0500

    questions 1 and 2

---
+++ baseball_script.sql
@@ -0,0 +1,20 @@
+SELECT MIN(debut) AS first_played,
+	MAX(finalgame) AS last_played
+FROM people;
+
+-- Range of games in the dataset is May 5 1871 to April 3 2017
+
+SELECT namefirst,
+	namelast,
+	MIN(height) AS shortest_height,
+	debut,
+	teams.name,
+	g_all
+FROM people
+INNER JOIN appearances USING (playerid)
+INNER JOIN teams USING (teamid)
+GROUP BY namefirst, namelast, debut, teams.name, g_all
+ORDER BY shortest_height
+LIMIT 1
+
+-- Eddie Gaedel, was the shortest man to play baseball at 43 inches. He played 1 games for the STL Browns
