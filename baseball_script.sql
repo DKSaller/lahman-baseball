3b80b54182f3a054088d6f8e91fcf4ba9edf6349
Donald Saller <donaldksaller@gmail.com>
2025-05-01 14:01:31 -0500

    question 3

--- baseball_script.sql
+++ baseball_script.sql
@@ -15,6 +15,20 @@ INNER JOIN appearances USING (playerid)
 INNER JOIN teams USING (teamid)
 GROUP BY namefirst, namelast, debut, teams.name, g_all
 ORDER BY shortest_height
-LIMIT 1
+LIMIT 1;
 
 -- Eddie Gaedel, was the shortest man to play baseball at 43 inches. He played 1 games for the STL Browns
+
+SELECT namefirst AS first_name,
+	namelast AS last_name,
+	SUM(salary) AS total_salary
+FROM people
+INNER JOIN collegeplaying USING (playerid)
+INNER JOIN schools USING (schoolid)
+INNER JOIN salaries USING (playerid)
+WHERE schoolid = 'vandy'
+GROUP BY namefirst, namelast
+ORDER BY total_salary DESC;
+
+-- Davide Price earned the most salary in the majors, earning $245,553,888 during his tenure
+
